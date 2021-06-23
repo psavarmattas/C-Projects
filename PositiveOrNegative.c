9947513ab9366359cc607c9c2e3488ef7789a3fe
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-06-23 16:52:39 -0700

    Changed it from "+" to ">"

--- PositiveOrNegative.c
+++ PositiveOrNegative.c
@@ -4,7 +4,7 @@ void main() {
     int num;
     printf("Input a number :");
     scanf("%d", &num);
-    if (num += 0)
+    if (num >= 0)
         printf("%d is a positive number \n", num);
     else if (num == 0) {
         printf("%d is zero \n", num);
